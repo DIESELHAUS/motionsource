e96c89c8ccc67f8092754212a9c65d5ffe4aa7da
Testing Dieselhaus <testing@dieselhaus.com>
2024-08-09 00:09:11 +0800

    update vimeo video

--- scripts/site.js
+++ scripts/site.js
@@ -215,10 +215,11 @@ $(document).ready(function() {
     var video =$(logo).data('video');
     var htmltitle= author+',<span class="info-auth-title"> '+title+'</span>';
 
+    if(video!="") {
     var parts = video.split('/');
     var vimeoID = parts.pop() || parts.pop();
-
     var datahtml = '<iframe src="https://player.vimeo.com/video/'+vimeoID.replace(/\s/g, "")+'?byline=0&portrait=0&title=0&autoplay=1"; allowfullscreen allow="autoplay; fullscreen" scrolling="no"></iframe>';
+    }
     
     $(testim).find(".nd-name").html(htmltitle);
     $(testim).find(".nd-text").html($(logo).data('text'));
