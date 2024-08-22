ab5f8e7a2e4c72181ce91827b71d2fdb35a712b6
Testing Dieselhaus <testing@dieselhaus.com>
2024-08-22 17:02:33 +0800

    add is active to 2nd child

--- scripts/site.js
+++ scripts/site.js
@@ -202,6 +202,8 @@ $(document).ready(function() {
 //    $(this).html(tonormal);
 //  });
 
+$(".nd-testimonials-one:nth-child(2)").addClass('is-active');
+
   $(".nd-testimonials-one").click(function(event) {
     var logo = $(this).find('.logo');
     var wrap = $(this).parents(".nd-testimonials");
